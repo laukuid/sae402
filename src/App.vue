87079757fc51b544f22e12adeb5d4c70de01bef7
guillaume <guiguit2004@gmail.com>
2024-04-03 16:25:26 +0200

    guillaume push

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,87 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+
+// Heure de départ à partir de laquelle l'horloge commencera à avancer
+const startTime = new Date();
+startTime.setHours(8, 0, 0, 0);
+
+// Formatage de l'heure
+const formattedTime = ref(getFormattedTime(startTime));
+
+// Mettre à jour l'heure chaque seconde
+setInterval(() => {
+  startTime.setSeconds(startTime.getSeconds() + 1);
+  formattedTime.value = getFormattedTime(startTime);
+}, 1000);
+
+// Fonction pour formater l'heure
+function getFormattedTime(time) {
+  const hours = time.getHours() < 10 ? time.getHours().toString().padStart(2, ' ') : time.getHours();
+  const minutes = String(time.getMinutes()).padStart(2, '0');
+  const seconds = String(time.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/notes">About</RouterLink>
+      <p> {{ formattedTime  }} </p>
+      <div class="HeaderIcon">
+        <img src="../src/assets/reseau.svg">
+        <img src="../src/assets/ios-wifi.svg">
+        <img src="../src/assets/battery.svg">
+      </div>
+    </nav>
   </header>
 
   <RouterView />
+  <footer>
+    <RouterLink to="/"><span></span></RouterLink>
+  </footer>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style scoped lang="scss">
+header{
+  display: flex;
+  align-items: center;
+  height: 35px;
+  width: calc(100% - 40px);
+  padding: 0 20px;
+  color: white;
+  nav{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: white;
+    .HeaderIcon{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 8px;
+    }
+  }
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+footer{
+  position: absolute;
+  bottom: 0;
+  left: 0;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  height: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  a{
+    span{
+      display: inline-block;
+      width: 155px;
+      height: 6px;
+      border-radius: 5px;
+      background-color: black;
+    }
   }
 }
 </style>
